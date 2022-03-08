73d882e93500616ee5a6f273d8ad8584d2c473ea
Jonah Cockshaw <58091924+jonahcockshaw@users.noreply.github.com>
2022-03-08 10:59:26 +0000

    Added Console Logs for Debugging and fixed an error in the calculation

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ function printOutputs() {
     // Declare all fixed variables 
     const bbpSavingTarget = 0.75;
     const bbpMinimumSavingCost = 100000;
-    const baselineElectricPrice = 0.28;
+    const baselineElectricPrice = 0.114;
     const baselineAverageChillerPlantLoad = 0.35;
     const estimatedChillerCapacityLoss = 0.15;
-    const baselineCO2 = 0.50;
+    const baselineCO2 = 0.554;
     const pumpDesignOverCapacity = 0.20;
 
     // Get values from inputs
@@ -28,20 +28,33 @@ function printOutputs() {
         return total > 0 & total != NaN;
     }
 
-    // Chiller 1 Calculations
+    // Chiller Calculations
     const nominalCapacity = allChillersValue;
     const nominalPower = nominalCapacity / 1.4;
     const numOfChillers = chillerList.filter(countChillers).length;
     console.log(numOfChillers);
     const chillerEfficiency = nominalPower / nominalCapacity;
     const correctedCapacity = nominalCapacity * (1 - estimatedChillerCapacityLoss);
-    const correctedChillerEfficiency = nominalPower / chillerEfficiency;
+    const correctedChillerEfficiency = nominalPower / correctedCapacity;
     const nominalTotalCapacity = nominalCapacity * numOfChillers;
     const correctedTotalCapacity = correctedCapacity * numOfChillers;
     const yearlyEnergyUsage = correctedTotalCapacity * correctedChillerEfficiency * 365 * 24 * baselineAverageChillerPlantLoad;
 
+        // Console Logs of Chiller Calculations
+        console.log(">>> Chiller Outputs <<<")
+        console.log('Nominal Capacity is ' + nominalCapacity);
+        console.log('Nominal Power is ' + nominalPower);
+        console.log('Number of Chillers is ' + numOfChillers);
+        console.log('Chiller Efficiency is ' + chillerEfficiency);
+        console.log('Corrected Capacity is ' + correctedCapacity);
+        console.log('Corrected Chiller Efficiency is ' + correctedChillerEfficiency);
+        console.log('Nominal Total Capacity is ' + nominalTotalCapacity);
+        console.log('Corrected Total Capacity is ' + correctedTotalCapacity);
+        console.log('Yearly Energy Usage is ' + yearlyEnergyUsage);
+
     // Chillers Totals
     const nominalCapacityOfAllChillers = (chiller1Value + chiller2Value + chiller3Value + chiller4Value + chiller5Value);
+    console.log('Nominal Capacity of All Chillers is ' + nominalCapacityOfAllChillers);
 
     // Condenser pump(s) Calculations
     const condenserNominalTotalPower = (14.0148778886542 + 0.11630564 * nominalPower) * numOfChillers;
@@ -49,11 +62,25 @@ function printOutputs() {
     const condenserPumpsYearlyEnergyUsage = condenserNominalTotalPowerCorrectedToLoad * 24 * 365 * baselineAverageChillerPlantLoad;
     const condenserPumpEfficiency = condenserNominalTotalPowerCorrectedToLoad / nominalTotalCapacity;
 
+        // Console Logs for Pump Calculations
+        console.log(">>> Condenser Pump Outputs <<<")
+        console.log('Condenser Nominal Total Power is ' + condenserNominalTotalPower);
+        console.log('Condenser Nominal Total Power Correct to Load is ' + condenserNominalTotalPowerCorrectedToLoad);
+        console.log('Condenser Pump Yearly Energy Usage is ' + condenserPumpsYearlyEnergyUsage);
+        console.log('Condenser Pump Efficiency ' + condenserPumpEfficiency);
+
     // Primary Pump(s) Calculations
     const primaryNominalTotalPower = (14.7360878094534 + 0.02728417 * nominalPower) * numOfChillers;
     const primaryNominalTotalPowerCorrectedToLoad = primaryNominalTotalPower * (1 - pumpDesignOverCapacity);
     const primaryPumpsYearlyEnergyUsage = primaryNominalTotalPowerCorrectedToLoad * 24 * 265 * baselineAverageChillerPlantLoad;
     const primaryPumpEfficiency = primaryNominalTotalPowerCorrectedToLoad / nominalTotalCapacity;
+
+        // Console Logs for Primary Pump
+        console.log(">>> Primary Pump Outputs <<<")
+        console.log('Primary Nominal Total Power is ' + primaryNominalTotalPower);
+        console.log('Primary Nominal Total Power Corrected to Load is ' + primaryNominalTotalPowerCorrectedToLoad);
+        console.log('Primary Pumps Yearly Energy Usage is ' + primaryPumpsYearlyEnergyUsage);
+        console.log('Primary Pumps Efficiency is ' + primaryPumpEfficiency);
 
     // Secondary Pump(s) Calculations
     const secondaryNominalTotalPower = (-28.8607474364754 + 0.26194674 * nominalPower) * numOfChillers;
@@ -61,10 +88,23 @@ function printOutputs() {
     const secondaryPumpsYearlyEnergyUsage = secondaryNominalTotalPowerCorrectedToLoad * 24 * 365 * baselineAverageChillerPlantLoad;
     const secondaryPumpEfficiency = secondaryNominalTotalPowerCorrectedToLoad / nominalTotalCapacity;
 
+        // Console Logs for Secondary Pump
+        console.log(">>> Secondary Pump Outputs <<<")
+        console.log('Secondary Nominal Total Power is ' + secondaryNominalTotalPower);
+        console.log('Secondary Nominal Total Power Corrected to Load is ' + secondaryNominalTotalPowerCorrectedToLoad);
+        console.log('Secondary Pumps Yearly Energy Usage is ' + secondaryPumpsYearlyEnergyUsage);
+        console.log('Secondary Pumps Efficiency is ' + secondaryPumpEfficiency);
+
     // Cooling Tower(s) Calculations
     const coolingTowerNominalTotalPower = (-4.64339871663608 + 0.06993997 * nominalPower) * numOfChillers;
     const coolingTowerPumpsYearlyEnergyUsage = coolingTowerNominalTotalPower * 24 * 365 * baselineAverageChillerPlantLoad;
     const coolingTowerEfficiency = coolingTowerNominalTotalPower / nominalTotalCapacity;
+    
+        // Console Logs for Cooling Towers
+        console.log(">>> Cooling Tower Outputs <<<")
+        console.log('Cooling Tower Nominal Total Power is ' + coolingTowerNominalTotalPower);
+        console.log('Cooling Tower Pumps Yearly Energy Usage is ' + coolingTowerPumpsYearlyEnergyUsage);
+        console.log('Cooling Tower Efficiency is ' + coolingTowerEfficiency);
 
     // Summary Calculations
         // in kWh
@@ -80,9 +120,17 @@ function printOutputs() {
         // in usd/year
     const electricSavingPotentialUsd = electricSavingPotentialKwh * baselineElectricPrice; 
 
+        // Console Logs for Summary Calculations
+        console.log(">>> Summary Calculation Outputs <<<")
+        console.log('Yearly Electric Consumption is ' + yearlyElectricityConsumption);
+        console.log('Total Electricity Consumption is ' + totalElectricityConsumption);
+        console.log('Calculated Plant Baseline Efficiency is ' + calculatedPlantBaselineEfficiency);
+        console.log('Electric Saving Potential kWh is ' + electricSavingPotentialKwh);
+        console.log('Co2 Saving Potential is ' + co2SavingPotential);
+        console.log('Electric Saving Potential USD ' + electricSavingPotentialUsd);
+
     // Print Outputs
     document.getElementById("cost-save").innerHTML = electricSavingPotentialUsd.toFixed(2);
     document.getElementById("electric-save").innerHTML = electricSavingPotentialKwh.toFixed(2);
     document.getElementById("co2-save").innerHTML = co2SavingPotential.toFixed(2);
-    console.log('hello world');
 };
